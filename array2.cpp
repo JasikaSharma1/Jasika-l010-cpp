be0ea2d01c27b82334c840e4da1968c03cfc095e
JasikaSharma1 <2024a1l010@mietjammu.in>
2025-03-03 12:23:17 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,23 @@
+/* Program to demeonstrate working of array of object(Method1)*/
+// #include<iostream>
+// using namespace std;
+// class program{       //class declaration
+//     private : int Regno; float Marks;    //Private member of a class
+//     public : Program(int R, float M){    //Paramerterized constructor of a class 
+//         Regno = R;
+//         Marks = M;
+//     }
+//     public : void display()       //Function of a class
+//     {
+//         cout<<"\n RegNumber ="<<Regno"\t"<<"Marks ="<<Marks;
+//     }
+//     };
+//     int main(){
+//         Program obj[3]= {101,450},{102,430},{103,440};       // Array of object with manual initialization
+//         int i;
+//         for(i=0;i<3;i++)
+//         {
+//             obj[i].Display();    // calling of Display Funcyion with array of objects
+//         }
+//     }
+    
